8cea71c4a96b7dc8daacddb9f38700ae56356760
ElSuperMagikarp <uriens2006@gmail.com>
2025-03-11 12:20:25 +0100

    Class Jugador added

---
+++ Jugador.java
@@ -0,0 +1,11 @@
+package UNO;
+
+import java.util.ArrayList;
+
+public class Jugador {
+    private String nom;
+    private ArrayList<Carta> cartes;
+
+    public String getNom(){return nom;}
+    public ArrayList<Carta> getCartes(){return cartes;}
+}
