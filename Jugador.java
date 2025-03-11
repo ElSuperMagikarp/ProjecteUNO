ecf8d53968bf155ec4a6fa87d97e65af2a46b908
ElSuperMagikarp <uriens2006@gmail.com>
2025-03-11 12:43:21 +0100

    Jugador Constructor Added

--- Jugador.java
+++ Jugador.java
@@ -3,8 +3,13 @@ package UNO;
 import java.util.ArrayList;
 
 public class Jugador {
-    private String nom;
+    private final String nom;
     private ArrayList<Carta> cartes;
+
+    public Jugador (String n){
+        nom = n;
+        cartes = new ArrayList<>();
+    }
 
     public String getNom(){return nom;}
     public ArrayList<Carta> getCartes(){return cartes;}
