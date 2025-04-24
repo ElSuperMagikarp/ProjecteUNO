ac97202c19418374cc45996ed8f716484533dd59
ElSuperMagikarp <uriens2006@gmail.com>
2025-04-24 13:30:25 +0200

    mostrarCartes on UI now shows numbers

--- interficie/UI.java
+++ interficie/UI.java
@@ -62,19 +62,29 @@ public class UI {
     public static void mostrarCartes(ArrayList<Carta> cartes) {
         int quantitat = cartes.size();
         String[][] cartesPintades = new String[quantitat][];
-    
+
         for (int i = 0; i < quantitat; i++) {
             cartesPintades[i] = pintarCarta(cartes.get(i)).split("\n");
         }
-    
-        int altura = ALTURA_CARTA;
-    
+
+        int altura = cartesPintades[0].length;
+
         for (int i = 0; i < altura; i++) {
             for (int j = 0; j < quantitat; j++) {
                 System.out.print(cartesPintades[j][i] + "  ");
             }
             System.out.println();
         }
+        System.out.print("    ");
+        for (int j = 0; j < quantitat; j++) {
+            if (j+1<10) {
+                System.out.print("(" + (j+1) +")          ");
+            }
+            else {
+                System.out.print("(" + (j+1) +")         ");
+            }
+        }
+        System.out.println();
     }
 
     public static ArrayList<String> demanarJugadors() {
