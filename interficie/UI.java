e067c54ca2fcf6800ebc1982670d17f5704b33cb
ElSuperMagikarp <uriens2006@gmail.com>
2025-04-24 13:13:54 +0200

    UI Player Interaction Added

--- interficie/UI.java
+++ interficie/UI.java
@@ -1,10 +1,12 @@
 package uno.interficie;
 
-import uno.logica.Carta;
-
+import uno.logica.*;
 import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class UI {
+    private static Scanner input = new Scanner(System.in);
     // ANSI escape codes 
     public static final String RESET = "\u001B[0m"; 
     public static final String RED = "\u001B[31m"; 
@@ -74,5 +76,62 @@ public class UI {
             System.out.println();
         }
     }
-    
+
+    public static ArrayList<String> demanarJugadors() {
+        System.out.print("Quanta gent jugará?: ");
+        int quantitatJugadors = input.nextInt();
+        input.nextLine();
+        ArrayList<String> llistaNoms = new ArrayList<>();
+        for (int i=1; i<quantitatJugadors+1; i++) {
+            System.out.print("Introdueix el nom del jugador "+i+": ");
+            String nomJugador = input.nextLine();
+            llistaNoms.add(nomJugador);
+        }
+        System.out.println();
+        return llistaNoms;
+    }
+
+    public static void tornJugador(Jugador jugador, Pilo pilo) {
+        System.out.println("És el torn de "+jugador.getNom());
+        mostrarUltimaCarta(pilo);
+    }
+
+    public static void senseCartes() {
+        System.out.println("No tens cartes que puguis tirar. Robant una carta...");
+    }
+
+    public static void mostrarMa(Jugador jugador) {
+        System.out.println("LA TEVA MÁ:");
+        mostrarCartes(jugador.getCartes());
+    }
+
+    public static void mostrarUltimaCarta(Pilo pilo) {
+        System.out.println("ULTIMA CARTA DEL PILÓ");
+        mostrarCarta(pilo.consultarCarta());
+        System.out.println();
+    }
+
+    public static Carta demanarCarta(Jugador jugador, Pilo pilo) {
+        mostrarMa(jugador);
+        do {
+            System.out.print("Escull una carta: ");
+            int indexCartaEscollida = input.nextInt();
+
+            if (indexCartaEscollida > 0 && indexCartaEscollida <= jugador.nombreDeCartes()) {
+                Carta cartaEscollida = jugador.getCartes().get(indexCartaEscollida-1);
+
+                if (Regles.sonCartesCompatibles(cartaEscollida, pilo.consultarCarta())){
+                    return cartaEscollida;
+                } else {
+                    System.out.println("Aquesta carta no coincideix amb l'última del pilo!");
+                }
+            } else {
+                System.out.println("ERROR: Has escollit una posició no vàlida");
+            }
+        } while (true);
+    }
+
+    public static void victoria(Jugador jugador) {
+        System.out.println(jugador.getNom()+" ha guanyat la partida!");
+    }
 }
